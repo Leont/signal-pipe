61294415b18749ec8eb566c653101db90f4c03c1
Leon Timmermans <fawaka@gmail.com>
2015-11-04 22:48:33 +0100

    Initial code

--- lib/Signal/Pipe.pm
+++ lib/Signal/Pipe.pm
@@ -3,8 +3,13 @@ package Signal::Pipe;
 use strict;
 use warnings;
 
+use Exporter 5.57 'import';
+our @EXPORT = 'selfpipe';
+
 use XSLoader;
 
 XSLoader::load(__PACKAGE__, __PACKAGE__->VERSION);
 
 1;
+
+# ABSTRACT: Self pipes for signal handling
--- lib/Signal/Pipe.xs
+++
@@ -1,9 +0,0 @@
-#define PERL_NO_GET_CONTEXT
-#include "EXTERN.h"
-#include "perl.h"
-#include "XSUB.h"
-
-MODULE = Signal::Pipe				PACKAGE = Signal::Pipe
-
-PROTOTYPES: DISABLED
-
